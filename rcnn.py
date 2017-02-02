72297237d52fcec966797fdc2cc0d16631da12a3
redsphinx <gabi.ras@hotmail.com>
2017-02-02 18:07:03 +0000

    split data in train and validation

--- rcnn.py
+++ rcnn.py
@@ -12,28 +12,37 @@ NUMBER_OF_TRACKLETS = len(os.listdir(DATA_DIRECTORY)) - 1
 VALIDATION_SIZE = 50
 NUM_LABELS = 2
 
-# dynamic variables, depends on the person: image width, height and number of frames.
-# determine these when loading the data
 
-def make_balanced_dataset():
+# make the dataset balanced
+def balance_data():
     labels_ = DATA_DIRECTORY + '/pair_labels.csv'
     labels = pd.read_csv(labels_)
     labels = labels.sort_values('class')
-    labels_ = labels.to_csv('labels_sorted.csv', index=False)
+    labels.to_csv('labels_sorted.csv', index=False)
+    # now labels are sorted with class 0 on top and 1 at the bottom
     with open('labels_sorted.csv') as f:
         reader = csv.reader(f)
         labels = list(reader)
-    # print(len(labels))
-    # print(labels[0])
-    # print(labels[-1])
-    # print(labels[len(labels)-10:-1])
     labels_neg = labels[1:len(labels)-200]
     rd.shuffle(labels_neg)
-    balanced_labels = labels_neg[0:200] + labels[len(labels)-201:-1]
+    labels_pos = labels[len(labels)-201:-1]
+    rd.shuffle(labels_pos)
+    balanced_labels = labels_neg[0:200] + labels_pos
+    rd.shuffle(balanced_labels)
+    return balanced_labels
+
+
+# create the validation dataset
+def create_data():
+    labels = balance_data()
+    print(labels[-1])
+    train = labels[0:len(labels)-VALIDATION_SIZE]
+    validation = labels[len(labels)-VALIDATION_SIZE-1:len(labels)]
+    return [train, validation]
+
 
 def main():
     # extract data into numpy arrays
     pass
 
-
-make_balanced_dataset()
+create_data()
