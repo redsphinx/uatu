1ad2e4820338dd98eeb6fc878dddf143fa8014e1
redsphinx <gabi.ras@hotmail.com>
2017-02-08 16:38:35 +0000

    file with python stuff for testing

--- testing_stuff.py
+++ testing_stuff.py
@@ -1,16 +1,49 @@
 import tensorflow as tf
 
-path = '/home/gabi/Documents/datasets/humans/1/per00001.jpg'
 
-file_queue = tf.train.string_input_producer([path])
-print(type(file_queue))
+def test_data_pipeline():
+    path = '/home/gabi/Documents/datasets/humans/1/per00001.jpg'
 
-reader = tf.WholeFileReader()
-print(type(reader))
+    file_queue = tf.train.string_input_producer([path])
+    print(type(file_queue))
 
-key, value = reader.read(file_queue)
-print(key)
-print(value)
+    reader = tf.WholeFileReader()
+    print(type(reader))
 
-my_img = tf.image.decode_png(value)
-print(type(my_img))
+    key, value = reader.read(file_queue)
+    print(key)
+    print(value)
+
+    my_img = tf.image.decode_png(value)
+    print(type(my_img))
+
+
+def test_perm():
+    x_before = tf.constant([[[1, 2, 3], [4,5,6]], [[11, 22, 33], [44,55,66]]])
+    x_after_1 = tf.transpose(x_before, perm=[1, 0, 2])
+    x_after_2 = tf.transpose(x_before, perm=[0, 2, 1])
+    x_after_3 = tf.transpose(x_before, perm=[0, 1, 2])
+    x_after_4 = tf.transpose(x_before, perm=[1,2,0])
+
+    init = tf.global_variables_initializer()
+
+    with tf.Session() as sess:
+        sess.run(init)
+        print(str('original: ')+ '\n' + str(tf.Tensor.eval(x_before)) + '\n')
+        print(str('1 0 2 ') + '\n'+ str(tf.Tensor.eval(x_after_1))+ '\n')
+        print(str('0 2 1 ') + '\n'+ str(tf.Tensor.eval(x_after_2))+ '\n')
+        print(str('0 1 2 ') + '\n'+ str(tf.Tensor.eval(x_after_3))+ '\n')
+        print(str('1 2 0 ') + '\n'+ str(tf.Tensor.eval(x_after_4))+ '\n')
+
+
+def test_reshape():
+    r = tf.constant([[1, 2, 3], [4,5,6]])
+    r_after = tf.reshape(r, [-1])
+
+    init = tf.global_variables_initializer()
+    with tf.Session() as sess:
+        sess.run(init)
+        print(tf.Tensor.eval(r_after))
+
+
+test_reshape()
