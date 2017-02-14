a032e197676527ac6446fb120125c65778de53e0
redsphinx <gabi.ras@hotmail.com>
2017-02-14 17:13:47 +0000

    training half done

--- siamese_cnn.py
+++ siamese_cnn.py
@@ -6,8 +6,10 @@ from PIL import Image
 
 def load_data():
     imarray = np.random.rand(10, 5, 3) * 255
-    im = Image.fromarray(imarray.astype('uint8')).convert('RGB')
-    return im
+    im1 = Image.fromarray(imarray.astype('uint8')).convert('RGB')
+    imarray = np.random.rand(10, 5, 3) * 255
+    im2 = Image.fromarray(imarray.astype('uint8')).convert('RGB')
+    return (im1, im2)
 
 
 def build_cnn(data, scope_name):
@@ -114,14 +116,11 @@ def build_cnn(data, scope_name):
 
 
 
-def build_model_siamese_cnn(data):
-    train_node = tf.placeholder(pc.DATA_TYPE, shape=[1, 10, 5, 3])
-
+def build_model_siamese_cnn(train_node_1, train_node_2):
     with tf.variable_scope('cnn_models') as scope:
-        model_1 = build_cnn(train_node, 'cnn_model_1')
+        model_1 = build_cnn(train_node_1, 'cnn_model_1')
         scope.reuse_variables()
-        model_2 = build_cnn(train_node, 'cnn_model_2')
-
+        model_2 = build_cnn(train_node_2, 'cnn_model_2')
 
     # define fc layers
     fc_1_weights = tf.get_variable(
@@ -153,21 +152,67 @@ def build_model_siamese_cnn(data):
 
     # get the distance between the 2 features
     distance = tf.sqrt(tf.reduce_sum(tf.pow(tf.sub(model_1, model_2), 2), 1, keep_dims=True))
-    print(tf.shape(distance))
     distance = tf.reshape(distance, [1,256])
-    print(tf.shape(distance))
 
     fc_1 = tf.nn.relu(tf.matmul(distance, fc_1_weights) + fc_1_biases)
     return tf.matmul(fc_1, fc_2_weights) + fc_2_biases
 
 
-
-data = np.asarray(load_data(), dtype=np.float32)
-
-print(np.shape(data))
-build_model_siamese_cnn(data)
-
-
 def main():
+    with tf.variable_scope('train-test') as scope:
+        data = load_data()
+        # training
+        train_node_1 = tf.placeholder(pc.DATA_TYPE, shape=[1, 10, 5, 3])
+        train_node_2 = tf.placeholder(pc.DATA_TYPE, shape=[1, 10, 5, 3])
+        train_label_node = tf.placeholder(pc.DATA_TYPE, shape=[1, 2]) # needs to be OHE
+
+        # validation
+        validation_node_1 = tf.placeholder(pc.DATA_TYPE, shape=[1, 10, 5, 3])
+        validation_node_2 = tf.placeholder(pc.DATA_TYPE, shape=[1, 10, 5, 3])
+        validation_label_node = tf.placeholder(pc.DATA_TYPE, shape=[1])
+
+        logits = build_model_siamese_cnn(train_node_1, train_node_2)
+        loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+            logits, train_label_node))
+
+        # TODO: maybe put regularizers and dropout
+
+        # step = 0
+        step = tf.Variable(0, dtype=pc.DATA_TYPE)
+        # Decay once per epoch
+        learning_rate = tf.train.exponential_decay(
+            pc.START_LEARNING_RATE,
+            step * pc.BATCH_SIZE,
+            pc.DECAY_STEP,
+            pc.DECAY_RATE,
+            staircase=True)
+        # Use simple momentum for the optimization.
+        optimizer = tf.train.MomentumOptimizer(learning_rate, pc.MOMENTUM)
+        optimizer = optimizer.minimize(loss, global_step=step)
+
+        # Predictions for the current training minibatch.
+        train_prediction = tf.nn.softmax(logits)
+
+        # Predictions for the test and validation, which we'll compute less often.
+        scope.reuse_variables()
+        validation_prediction = tf.nn.softmax(build_model_siamese_cnn(validation_node_1, validation_node_2))
+
+        # running everything
+        if tf.__version__ == '0.10.0':
+            init = tf.initialize_all_variables()
+        elif tf.__version__ == '0.12.0':
+            init = tf.global_variables_initializer()
+        else:
+            print('version not supported. add what to do manually.')
+            init = tf.global_variables_initializer()
+
+        with tf.Session() as sess:
+            sess.run(init)
+            epoch = 0
+            print('yay gabi')
+
 
     pass
+
+
+main()
