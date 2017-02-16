ba6409c0753ff44a2abcc885f690b6c691caf257
redsphinx <gabi.ras@hotmail.com>
2017-02-16 15:00:02 +0000

    completed data loading

--- src/project_utils.py
+++ src/project_utils.py
@@ -2,6 +2,9 @@ import numpy as np
 import project_constants as pc
 from PIL import Image
 import os
+import random as rd
+from scipy import ndimage
+
 
 def tupconv(lst):
     tuplst = []
@@ -72,49 +75,78 @@ def crop_images(folder_path, width, height):
 
 
     list_images = os.listdir(folder_path)
-    # overwriting original images
-    name_folder = folder_path.split('/')[-2]
+    name_folder = folder_path.split('/')[-num]
     new_folder_path = os.path.join(new_path, 'real_cropped_images_' + str(name_folder))
+    new_folder_path = '/' + new_folder_path
+    if not os.path.exists(new_folder_path):
+        print('asdf')
+        os.makedirs(new_folder_path)
 
     for image_path in list_images:
-        img = Image.open(image_path)
+        img = Image.open(os.path.join(folder_path, image_path))
         img_width, img_height = img.size
-        center_x = # TODO finish this
-        start_x =
-        start_y = 0
-        img2 = img.crop(start_x, start_y, width, height)
+
+
+        center_x = img_width / 2
+        center_y = img_height / 2
+        start_x = center_x - width / 2
+        start_y = center_y - height / 2
+        img2 = img.crop((start_x, start_y, start_x + width, start_y + height))
+        img2.save(os.path.join(new_folder_path, image_path))
 
     pass
 
 
 def load_INRIA_data(path):
 
+    print('loading data')
+    # load into lists
+    train_data_names_pos_ = np.genfromtxt(os.path.join(path, 'train_data_names_pos.csv'), dtype=None).tolist()
+    train_data_names_neg_ = np.genfromtxt(os.path.join(path, 'train_data_names_neg.csv'), dtype=None).tolist()
+    train_data_labels_pos_ = np.genfromtxt(os.path.join(path, 'train_data_labels_pos.csv'), dtype=None).tolist()
+    train_data_labels_neg_ = np.genfromtxt(os.path.join(path, 'train_data_labels_neg.csv'), dtype=None).tolist()
+    validation_data_names_pos_ = np.genfromtxt(os.path.join(path, 'validation_data_names_pos.csv'), dtype=None).tolist()
+    validation_data_names_neg_ = np.genfromtxt(os.path.join(path, 'validation_data_names_neg.csv'), dtype=None).tolist()
+    validation_data_labels_pos_ = np.genfromtxt(os.path.join(path, 'validation_data_labels_pos.csv'), dtype=None).tolist()
+    validation_data_labels_neg_ = np.genfromtxt(os.path.join(path, 'validation_data_labels_neg.csv'), dtype=None).tolist()
+
+    # TODO (optional) if images need cropping, use the 'crop_images()' function above separately
+
+    # shuffle
+    train_data_ = train_data_names_pos_ + train_data_names_neg_
+    train_labels_ = train_data_labels_pos_ + train_data_labels_neg_
+
+    validation_data_ = validation_data_names_pos_ + validation_data_names_neg_
+    validation_labels_ = validation_data_labels_pos_ + validation_data_labels_neg_
+
+    everything = list(zip(train_data_, train_labels_))
+    rd.shuffle(everything)
+    train_data_, train_labels_ = zip(*everything)
+
+    everything = list(zip(validation_data_, validation_labels_))
+    rd.shuffle(everything)
+    validation_data_, validation_labels_ = zip(*everything)
+
+    # create empty arrays
+    train_data_array = np.zeros(shape=(len(train_data_), pc.IMAGE_HEIGHT, pc.IMAGE_WIDTH, pc.NUM_CHANNELS))
+    train_labels_array = np.zeros(shape=(len(train_labels_), pc.NUM_CLASSES))
+    validation_data_array = np.zeros(shape=(len(validation_data_), pc.IMAGE_HEIGHT, pc.IMAGE_WIDTH, pc.NUM_CHANNELS))
+    validation_labels_array = np.zeros(shape=(len(validation_labels_), pc.NUM_CLASSES))
+
     def ohe(old, new):
         for item in range(0, len(old)):
             new[item][old[item]] = 1
         return new
 
-    print('loading data')
-    # load into lists
-    train_data_names_pos_ = np.genfromtxt(os.path.join(path, 'train_data_names_pos.csv'), dtype=None)
-    train_data_names_neg_ = np.genfromtxt(os.path.join(path, 'train_data_names_neg.csv'), dtype=None)
-    train_data_labels_pos_ = np.genfromtxt(os.path.join(path, 'train_data_labels_pos.csv'), dtype=None)
-    train_data_labels_neg_ = np.genfromtxt(os.path.join(path, 'train_data_labels_neg.csv'), dtype=None)
-    validation_data_names_pos_ = np.genfromtxt(os.path.join(path, 'validation_data_names_pos.csv'), dtype=None)
-    validation_data_names_neg_ = np.genfromtxt(os.path.join(path, 'validation_data_names_neg.csv'), dtype=None)
-    validation_data_labels_pos_ = np.genfromtxt(os.path.join(path, 'validation_data_labels_pos.csv'), dtype=None)
-    validation_data_labels_neg_ = np.genfromtxt(os.path.join(path, 'validation_data_labels_neg.csv'), dtype=None)
-    # create empty arrays
-    train_data = np.zeros(shape=(len(train_data_names_pos_) + len(train_data_names_neg_),
-                                 pc.IMAGE_HEIGHT, pc.IMAGE_WIDTH, pc.NUM_CHANNELS))
-    train_labels = np.zeros(shape=(len(train_data_labels_pos_) + len(train_data_labels_neg_),
-                                 pc.NUM_CLASSES))
-    validation_data = np.zeros(shape=(len(validation_data_names_pos_) + len(validation_data_names_neg_),
-                                 pc.IMAGE_HEIGHT, pc.IMAGE_WIDTH, pc.NUM_CHANNELS))
-    train_labels = np.zeros(shape=(len(validation_data_labels_pos_) + len(validation_data_labels_neg_),
-                                   pc.NUM_CLASSES))
-    # fill the arrays
+    for image in range(0, len(train_data_)):
+        train_data_array[image] = ndimage.imread(train_data_[image])[:, :, 0:3]
+    for image in range(0, len(validation_data_)):
+        validation_data_array[image] = ndimage.imread(validation_data_[image])[:, :, 0:3]
 
+    train_labels = ohe(train_labels_, train_labels_array)
+    validation_labels = ohe(validation_labels_, validation_labels_array)
+
+    return [train_data_array, train_labels, validation_data_array, validation_labels]
     pass
 
 
@@ -133,7 +165,7 @@ def load_human_detection_data(name):
     if len(os.listdir(data_list_folder)) <= 0:
         print('folder empty, making files')
         # write list then load data
-        train_data_path_pos = os.path.join(location_path, train_path, 'pos')
+        train_data_path_pos = os.path.join(location_path, train_path, 'real_cropped_images_pos')
         train_data_names_pos = os.listdir(train_data_path_pos)
         with open(os.path.join(data_list_folder, 'train_data_names_pos.csv'), 'wr') as my_file:
             for item in train_data_names_pos:
@@ -143,7 +175,7 @@ def load_human_detection_data(name):
             for item in train_data_names_pos:
                 my_file.write(str(1) + '\n')
 
-        train_data_path_neg = os.path.join(location_path, train_path, 'neg')
+        train_data_path_neg = os.path.join(location_path, train_path, 'real_cropped_images_neg')
         train_data_names_neg = os.listdir(train_data_path_neg)
         with open(os.path.join(data_list_folder, 'train_data_names_neg.csv'), 'wr') as my_file:
             for item in train_data_names_neg:
@@ -153,7 +185,7 @@ def load_human_detection_data(name):
             for item in train_data_names_neg:
                 my_file.write(str(0) + '\n')
 
-        validation_data_path_pos = os.path.join(location_path, validation_path, 'pos')
+        validation_data_path_pos = os.path.join(location_path, validation_path, 'real_cropped_images_pos')
         validation_data_names_pos = os.listdir(validation_data_path_pos)
         with open(os.path.join(data_list_folder, 'validation_data_names_pos.csv'), 'wr') as my_file:
             for item in validation_data_names_pos:
@@ -163,7 +195,7 @@ def load_human_detection_data(name):
             for item in validation_data_names_pos:
                 my_file.write(str(1) + '\n')
 
-        validation_data_path_neg = os.path.join(location_path, validation_path, 'neg')
+        validation_data_path_neg = os.path.join(location_path, validation_path, 'real_cropped_images_neg')
         validation_data_names_neg = os.listdir(validation_data_path_neg)
         with open(os.path.join(data_list_folder, 'validation_data_names_neg.csv'), 'wr') as my_file:
             for item in validation_data_names_neg:
@@ -175,6 +207,8 @@ def load_human_detection_data(name):
 
     data = load_INRIA_data(data_list_folder)
     return data
+
+
     pass
 
-crop_images('asdf/as/dfas/dfas/df/as/dfas/df/gabi', 23, 23)
+load_human_detection_data('wer')
