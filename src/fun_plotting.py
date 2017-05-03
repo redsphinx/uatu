7921ab299a2892afa3a0a9aa2f796a01bc5f5754
redsphinx <gabi.ras@hotmail.com>
2017-05-03 11:11:44 +0100

    ran experiments

--- src/fun_plotting.py
+++ src/fun_plotting.py
@@ -4,49 +4,50 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-lr = [0.00001, 0.0001, 0.001]
-
-transfer_pos_precision = [0.2485, 0.2594, 0.1071]
-random_pos_precision = [0.4455, 0.6627, 0.3026]
-
-transfer_pos_recall = [0.6833, 0.9167, 0.7333]
-random_pos_recall = [0.75, 0.9167, 0.9583]
-
-pos_precision = [0.2485, 0.4455, 0.2594, 0.6627, 0.1071, 0.3026]
-pos_recall = [0.6833, 0.75, 0.9167, 0.9167, 0.7333, 0.9583]
+mean_ranking_viper_15 = np.array([ 0.545,  0.795,  0.905,  0.955,  0.975,  0.98 ,  0.99 ,  0.99 ,
+        0.99 ,  0.99 ,  1.   ,  1.   ,  1.   ,  1.   ,  1.   ,  1.   ,
+        1.   ,  1.   ,  1.   ,  1.   ])
 
 
-learning_rates = [0.00001, 0.00001, 0.00001, 0.00001,
-                  0.0001, 0.0001, 0.0001, 0.0001,
-                  0.001, 0.001, 0.001, 0.001]
-weights = ['transfer','transfer', 'random', 'random',
-         'transfer','transfer', 'random', 'random',
-         'transfer','transfer', 'random', 'random']
+mean_ranking_viper_40 = np.array([ 0.705,  0.915,  0.955,  0.97 ,  0.985,  0.995,  0.995,  0.995,
+        1.   ,  1.   ,  1.   ,  1.   ,  1.   ,  1.   ,  1.   ,  1.   ,
+        1.   ,  1.   ,  1.   ,  1.   ])
 
-values = [0.2485, 0.4455, 0.6833, 0.75,
-          0.2594, 0.6627, 0.9167, 0.9167,
-          0.1071, 0.3026, 0.7333, 0.9583]
+mean_ranking_cuhk_15 = np.array([ 0.42 ,  0.65 ,  0.785,  0.9  ,  0.915,  0.965,  0.975,  0.995,
+        0.995,  1.   ,  1.   ,  1.   ,  1.   ,  1.   ,  1.   ,  1.   ,
+        1.   ,  1.   ,  1.   ,  1.   ])
 
-
-
-
-plt.plot(lr, transfer_pos_precision, color='red', label='transfer')
-plt.plot(lr, random_pos_precision)
-
-plt.plot(lr, transfer_pos_recall)
-plt.plot(lr, random_pos_recall)
-
-plt.xlabel('learning rate')
+mean_ranking_cuhk_40 = np.array([ 0.605,  0.775,  0.88 ,  0.94 ,  0.97 ,  0.99 ,  0.995,  0.995,
+        1.   ,  1.   ,  1.   ,  1.   ,  1.   ,  1.   ,  1.   ,  1.   ,
+        1.   ,  1.   ,  1.   ,  1.   ])
 
 
-plt.show()
+def plot_CMC():
+    plt.figure(1)
+    plt.subplot(211)
+    plt.plot(range(1, 21), mean_ranking_viper_15 * 100, label='15 epochs')
+    plt.plot(range(1, 21), mean_ranking_viper_40 * 100, label='40 epochs')
+    plt.xticks(range(1, 21))
 
+    plt.xlabel('Rank')
+    plt.ylabel('Matching Rate')
+    plt.legend()
+    plt.title('VIPeR dataset')
 
-# precision = np.array([learning_rates, weights, values, units])
-# precision = np.transpose(precision)
-#
-# pre = pd.DataFrame(data=precision, columns=['learning rate', 'weights', 'values', 'dummy'])
-# # gammas = sns.load_dataset("gammas")
-#
-# sns.tsplot(pre, time='learning rate', value='values', condition='weights', unit='dummy')
-# sns.plt.show()
+    plt.subplot(212)
+    plt.plot(range(1,21), mean_ranking_cuhk_15*100, label='15 epochs')
+    plt.plot(range(1,21), mean_ranking_cuhk_40*100, label='40 epochs')
+
+    plt.xticks(range(1, 21))
+
+    plt.xlabel('Rank')
+    plt.ylabel('Matching Rate')
+    plt.legend()
+    plt.title('CUHK01 dataset')
+    plt.tight_layout()
+    # sns.palplot(sns.color_palette("hls", 2))
+
+    # plt.savefig('CMC_curve_viper_cihk01.png', format='png', dpi=400)
+    plt.show()
+
+plot_CMC()
