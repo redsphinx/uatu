e7e2a8b91ec5635fe965ef34a31da8f09dbf952a
redsphinx <gabi.ras@hotmail.com>
2017-06-23 19:19:03 +0100

    gui basics work

--- src/gui.py
+++ src/gui.py
@@ -1,55 +1,102 @@
-import gui_variables as gv
-from Tkinter import Button
+from gui_variables import GuiVariable
+from Tkinter import Button, Label
 
 
+def set_run_possible(gv):
+    if gv.is_model_loaded and gv.is_test_loaded:
+        if gv.run_button['state'] == 'disabled':
+            gv.run_button.config(state='active', command=lambda: run(gv))
+            gv.step_button.config(state='active', command=lambda: step(gv))
+            gv.reset_button.config(state='active', command=lambda: reset(gv))
+    else:
+        pass
 
-def load_model():
-    loaded_model_text = gv.loaded_model_text
-    loaded_model_text.config(text='model loaded')
-    run_conditions = 1
+
+def initialize(b):
+    b.loaded_model_text = Label(b.root, text='No model loaded')
+    b.loaded_test_text = Label(b.root, text='No test loaded')
+    b.bb_image_left =  Label(b.root, image=b.tk_bb_image)
+    b.bb_image_right =  Label(b.root, image=b.tk_bb_image)
+    b.run_button =  Button(b.root, text='Run', state='disabled')
+    b.step_button =  Button(b.root, text='Step', state='disabled')
+    b.reset_button =  Button(b.root, text='Reset', state='disabled')
+    b.certainty_text =  Label(b.root, text='1.00')
+    b.prediction_text = Label(b.root, text='Match')
+    b.load_model_button = Button(b.root, text='Load Model', command=lambda: load_model(b))
+    b.load_test_button = Button(b.root, text='Load Test', command=lambda: load_test(b))
+
+
+def load_model(gv):
+    gv.loaded_model_text.config(text='model loaded')
+    gv.is_model_loaded = True
     print('load model')
+    set_run_possible(gv)
 
 
-def load_test():
-    loaded_test_text = gv.loaded_test_text
-    loaded_test_text.config(text='test loaded')
+def load_test(gv):
+    gv.loaded_test_text.config(text='test loaded')
+    gv.is_test_loaded = True
     print('load test')
+    set_run_possible(gv)
 
 
-def run():
-    if gv.run_conditions == 2:
-        print('run')
+def run(gv):
+    print('run')
+
+    gv.load_model_button.config(state='disabled')
+    gv.load_test_button.config(state='disabled')
+
+    if gv.run_button['text'] == 'Run':
+        gv.run_button.config(text='Pause')
+    elif gv.run_button['text'] == 'Pause':
+        gv.run_button.config(text='Run')
+    else:
+        gv.run_button.config(text='???')
 
 
-def step():
+
+def step(gv):
+    if gv.run_button['text'] == 'Pause':
+        gv.run_button.config(text='Run')
     print('step')
 
 
-def stop():
-    print('stop')
+def reset(gv):
+    gv.run_button.config(text='Run', state='disabled')
+    gv.loaded_model_text.config(text='No model loaded')
+    gv.is_model_loaded = False
+    gv.loaded_test_text.config(text='No test loaded')
+    gv.is_test_loaded = False
+    gv.step_button.config(state='disabled')
+    gv.load_model_button.config(state='active')
+    gv.load_test_button.config(state='active')
+
+    gv.reset_button.config(state='disabled')
+
+    print('reset')
 
 
-def create_window(root):
+def create_window(gv):
     """ Creates a window and implements basic things
     """
     # set title and size and position
-    root.title(gv.window_title)
-    root.geometry('%dx%d+%d+%d' % (gv.window_width, gv.window_width, gv.window_position, gv.window_position))
+    gv.root.title(gv.window_title)
+    gv.root.geometry('%dx%d+%d+%d' % (gv.window_width, gv.window_width, gv.window_position, gv.window_position))
 
     # make button for loading model
-    load_model_button = Button(root, text='Load Model', command=load_model).grid(row=0, column=2)
+    load_model_button = gv.load_model_button.grid(row=0, column=2)
     # make text field indicating the selected model
     model_text = gv.loaded_model_text.grid(row=1, column=2)
     # make button for loading test
-    load_test_button = Button(root, text='Load Test', command=load_test).grid(row=2, column=2)
+    load_test_button = gv.load_test_button.grid(row=2, column=2)
     # make button for selecting test set
     load_test_text = gv.loaded_test_text.grid(row=3, column=2)
     # create run button
     run_button = gv.run_button.grid(row=4, column=2)
     # create step button
     step_button = gv.step_button.grid(row=5, column=2)
-    # create stop button
-    stop_button = gv.stop_button.grid(row=6, column=2)
+    # create reset button
+    reset_button = gv.reset_button.grid(row=6, column=2)
     # create dummy images
     image_left = gv.bb_image_left.grid(row=0, column=0)
     image_right = gv.bb_image_right.grid(row=0, column=1)
@@ -58,12 +105,13 @@ def create_window(root):
     # create prediction label
     prediction_text = gv.prediction_text.grid(row=6, column=1)
 
-    return root
+    return gv.root
 
 
 def main():
-    root = gv.root
-    root = create_window(root)
+    b = GuiVariable()
+    initialize(b)
+    root = create_window(b)
     root.mainloop()
 
 
--- src/gui_variables.py
+++ src/gui_variables.py
@@ -6,20 +6,25 @@ from Tkinter import Tk, Label, PhotoImage, Button
 class GuiVariable(object):
     def __init__(self):
         self._root = Tk()
+        self._window_title = 'uatu-0.0.0'
         self._window_width = 300
         self._window_height = 300
         self._window_position = 100
         self._run_conditions = 0
-        self._tk_bb_image= PhotoImage(file='/home/gabi/PycharmProjects/uatu/images/bb.png')
-        self._loaded_model_text = Label(root, text='No model loaded')
-        self._loaded_test_text = Label(root, text='No test loaded')
-        self._bb_image_left = Label(root, image=tk_bb_image)
-        self._bb_image_right = Label(root, image=tk_bb_image)
-        self._run_button = Button(root, text='Run', state='disabled')
-        self._step_button = Button(root, text='Step', state='disabled')
-        self._stop_button = Button(root, text='Stop', state='disabled')
-        self._certainty_text = Label(root, text='1.00')
-        self._prediction_text = Label(root, text='MATCH')
+        self._tk_bb_image = PhotoImage(file='/home/gabi/PycharmProjects/uatu/images/bb.png')
+        self._loaded_model_text = None   # Label(root, text='No model loaded')
+        self._loaded_test_text = None   # Label(root, text='No test loaded')
+        self._bb_image_left = None   # Label(root, image=tk_bb_image)
+        self._bb_image_right = None   # Label(root, image=tk_bb_image)
+        self._run_button = None   # Button(root, text='Run', state='disabled')
+        self._step_button = None   # Button(root, text='Step', state='disabled')
+        self._reset_button = None   # Button(root, text='Stop', state='disabled')
+        self._certainty_text = None   # Label(root, text='1.00')
+        self._prediction_text = None   # Label(root, text='MATCH')
+        self._is_model_loaded = False
+        self._is_test_loaded = False
+        self._load_model_button = None  # Button(gv.root, text='Load Model', command=lambda: load_model_button(gv)).grid(row=0, column=2)
+        self._load_test_button = None  # Button(gv.root, text='Load Test', command=lambda: load_test_button(gv)).grid(row=2, column=2)
 
     @property
     def root(self):
@@ -28,6 +33,14 @@ class GuiVariable(object):
     @root.setter
     def root(self, value):
         self._root = value
+
+    @property
+    def window_title(self):
+        return self._window_title
+
+    @window_title.setter
+    def window_title(self, value):
+        self._window_title = value
     
     # self._window_width = 300
     @property
@@ -57,16 +70,132 @@ class GuiVariable(object):
         self._window_position = value
 
     # self._run_conditions = 0
+    @property
+    def run_conditions(self):
+        return self._run_conditions
+
+    @run_conditions.setter
+    def run_conditions(self, value):
+        self._run_conditions = value
+
+    # self._tk_bb_image = PhotoImage(file='/home/gabi/PycharmProjects/uatu/images/bb.png')@property
+    @property
+    def tk_bb_image(self):
+        return self._tk_bb_image
+
+    @tk_bb_image.setter
+    def tk_bb_image(self, value):
+        self._tk_bb_image = value
         
-
-
-    # self._tk_bb_image = PhotoImage(file='/home/gabi/PycharmProjects/uatu/images/bb.png')
     # self._loaded_model_text = Label(root, text='No model loaded')
+    @property
+    def loaded_model_text(self):
+        return self._loaded_model_text
+
+    @loaded_model_text.setter
+    def loaded_model_text(self, value):
+        self._loaded_model_text = value
+        
     # self._loaded_test_text = Label(root, text='No test loaded')
+    @property
+    def loaded_test_text(self):
+        return self._loaded_test_text
+
+    @loaded_test_text.setter
+    def loaded_test_text(self, value):
+        self._loaded_test_text = value
+        
     # self._bb_image_left = Label(root, image=tk_bb_image)
+    @property
+    def bb_image_left(self):
+        return self._bb_image_left
+
+    @bb_image_left.setter
+    def bb_image_left(self, value):
+        self._bb_image_left = value
+
     # self._bb_image_right = Label(root, image=tk_bb_image)
+    @property
+    def bb_image_right(self):
+        return self._bb_image_right
+
+    @bb_image_right.setter
+    def bb_image_right(self, value):
+        self._bb_image_right = value
+
     # self._run_button = Button(root, text='Run', state='disabled')
+    @property
+    def run_button(self):
+        return self._run_button
+
+    @run_button.setter
+    def run_button(self, value):
+        self._run_button = value
+
     # self._step_button = Button(root, text='Step', state='disabled')
-    # self._stop_button = Button(root, text='Stop', state='disabled')
+    @property
+    def step_button(self):
+        return self._step_button
+
+    @step_button.setter
+    def step_button(self, value):
+        self._step_button = value
+
+    # self._reset_button = Button(root, text='Stop', state='disabled')
+    @property
+    def reset_button(self):
+        return self._reset_button
+
+    @reset_button.setter
+    def reset_button(self, value):
+        self._reset_button = value
+
     # self._certainty_text = Label(root, text='1.00')
-    # self._prediction_text = Label(root, text='MATCH')
+    @property
+    def certainty_text(self):
+        return self._certainty_text
+
+    @certainty_text.setter
+    def certainty_text(self, value):
+        self._certainty_text = value
+
+    # self._prediction_text = Label(root, text='MATCH')    @property
+    @property
+    def prediction_text(self):
+        return self._prediction_text
+
+    @prediction_text.setter
+    def prediction_text(self, value):
+        self._prediction_text = value
+        
+    @property
+    def is_model_loaded(self):
+        return self._is_model_loaded
+
+    @is_model_loaded.setter
+    def is_model_loaded(self, value):
+        self._is_model_loaded = value
+        
+    @property
+    def is_test_loaded(self):
+        return self._is_test_loaded
+
+    @is_test_loaded.setter
+    def is_test_loaded(self, value):
+        self._is_test_loaded = value
+        
+    @property
+    def load_model_button(self):
+        return self._load_model_button
+
+    @load_model_button.setter
+    def load_model_button(self, value):
+        self._load_model_button = value
+
+    @property
+    def load_test_button(self):
+        return self._load_test_button
+
+    @load_test_button.setter
+    def load_test_button(self, value):
+        self._load_test_button = value
