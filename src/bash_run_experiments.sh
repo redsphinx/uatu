b146f362045cd530cde5eef2d2a7e8b03a91fbe2
redsphinx <gabi.ras@hotmail.com>
2017-07-07 11:05:32 +0100

    new experiments ran

--- src/bash_run_experiments.sh
+++ src/bash_run_experiments.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 
-python running_experiments_3.py 1
-python running_experiments_3.py 2
-python running_experiments_3.py 3
-python running_experiments_3.py 4
-python running_experiments_3.py 5
-python running_experiments_3.py 6
-python running_experiments_3.py 7
-python running_experiments_3.py 8
-python running_experiments_3.py 9
-python running_experiments_3.py 10
-python running_experiments_3.py 11
-python running_experiments_3.py 12
+python running_experiments_2.py
+python running_experiments_2.py
+python running_experiments_2.py
+python running_experiments_2.py
