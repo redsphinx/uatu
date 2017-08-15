3501f87bf4e93f792eba8528146e77ab8ac9cf93
redsphinx <gabi.ras@hotmail.com>
2017-08-15 11:45:21 +0100

    results processed round 1

---
+++ processing_results/first_round/results-devbox-5-2/bash_1.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# using gpu 1
+
+python experiments_thesis.py 5_2_0
+python experiments_thesis.py 5_2_1
+python experiments_thesis.py 5_2_2
+python experiments_thesis.py 9_4
+
+#python experiments_thesis.py 10_2_1
+
+#python running_experiments_2.py 1_0
+#python running_experiments_2.py 1_1
+#python running_experiments_2.py 1_2
+
+#python experiments_thesis.py 10_0_0
+
+#python running_experiments_3.py 2_0_0
+#python running_experiments_3.py 2_0_1
+#python running_experiments_3.py 2_0_2
+#python running_experiments_3.py 2_1_0
+#python running_experiments_3.py 2_1_1
+#python running_experiments_3.py 2_1_2
+
+#python experiments_thesis.py 1_0_1
+#python experiments_thesis.py 1_1_1
+#python experiments_thesis.py 1_2_1
+#python experiments_thesis.py 1_3_1
+#python experiments_thesis.py 1_4_1
+#python experiments_thesis.py 1_5_1
+
+#python experiments_thesis.py 10_1_0
+#python experiments_thesis.py 10_1_1
+#python experiments_thesis.py 6_0_1
+#python experiments_thesis.py 6_1_1
+#python experiments_thesis.py 7_3
+#python experiments_thesis.py 7_4
+#python experiments_thesis.py 7_5
