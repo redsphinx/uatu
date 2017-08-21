7ea96b2f2fc14b46d21c6dc08928fb772ed67ad1
redsphinx <gabi.ras@hotmail.com>
2017-08-21 12:09:31 +0100

    results 9

---
+++ processing_results/second_round/results-devbox-9/bash_0.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+# using gpu 0
+
+python experiments_thesis.py 16_0_0
+python experiments_thesis.py 16_1_0
+python experiments_thesis.py 16_2_0
+python experiments_thesis.py 16_3_0
+python experiments_thesis.py 18_0_0
+python experiments_thesis.py 18_1_0
+python experiments_thesis.py 18_2_0
+python experiments_thesis.py 18_3_0
+python experiments_thesis.py 19_0_0
+python experiments_thesis.py 19_1_0
+python experiments_thesis.py 19_2_0
+python experiments_thesis.py 19_3_0
+
+#python experiments_thesis.py 15_0_0
+#python experiments_thesis.py 15_1_0
+#python experiments_thesis.py 15_2_0
+#python experiments_thesis.py 15_3_0
+#python experiments_thesis.py 14_2
+
+#python experiments_thesis.py 11_0
+#python experiments_thesis.py 11_1
+
+#python experiments_thesis.py 4_3_0
+#python experiments_thesis.py 4_3_1
+#python experiments_thesis.py 4_3_2
+
+#python experiments_thesis.py 4_2_0
+#python experiments_thesis.py 4_2_1
+#python experiments_thesis.py 4_2_2
+#python experiments_thesis.py 9_3
+
+#python experiments_thesis.py 10_2_0
+
+#python running_experiments_2.py 0_0
+#python running_experiments_2.py 0_1
+#python running_experiments_2.py 0_2
+
+#python experiments_thesis.py 6_0_0
+#python experiments_thesis.py 6_0_1
+#python experiments_thesis.py 6_0_2
+#python experiments_thesis.py 6_1_0
+#python experiments_thesis.py 6_1_1
+#python experiments_thesis.py 6_1_2
+#python experiments_thesis.py 7_0
+#python experiments_thesis.py 7_1
+#python experiments_thesis.py 7_2
+#python experiments_thesis.py 7_3
+#python experiments_thesis.py 7_4
+#python experiments_thesis.py 7_5
+
+#python experiments_thesis.py 1_0_0
+#python experiments_thesis.py 1_1_0
+#python experiments_thesis.py 1_2_0
+#python experiments_thesis.py 1_3_0
+#python experiments_thesis.py 1_4_0
+#python experiments_thesis.py 1_5_0
+
+#python experiments_thesis.py 10_0_0
+#python experiments_thesis.py 10_0_1
+#python experiments_thesis.py 6_0_0
+#python experiments_thesis.py 6_1_0
+#python experiments_thesis.py 7_0
+#python experiments_thesis.py 7_1
+#python experiments_thesis.py 7_2
