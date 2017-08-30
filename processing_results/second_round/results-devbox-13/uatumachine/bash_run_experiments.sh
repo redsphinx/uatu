4b12dc6915ce8ae57b028ede1705139e1272ffbe
redsphinx <gabi.ras@hotmail.com>
2017-08-30 13:50:14 +0100

    results

---
+++ processing_results/second_round/results-devbox-13/uatumachine/bash_run_experiments.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+python running_experiments_2.py 31_0
+python running_experiments_2.py 31_1
+python running_experiments_2.py 31_2
+python running_experiments_2.py 31_3
+python running_experiments_2.py 31_4
+python running_experiments_2.py 31_5
